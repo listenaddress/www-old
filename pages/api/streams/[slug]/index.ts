d624f4d3034ac9d2a0f72c8c1a5d59baf34ba08b
Thomas O'Brien <tbobrien612@gmail.com>
2023-06-29 12:14:01 -0500

    Another cors test

--- pages/api/streams/[slug]/index.ts
+++ pages/api/streams/[slug]/index.ts
@@ -7,6 +7,22 @@ const redis = new Redis({
     port: parseInt(process.env.REDIS_PORT!),
     password: process.env.REDIS_PASSWORD!
 });
+
+const allowCors = (fn: any) => async (req: any, res: any) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    return await fn(req, res)
+}
+
 const getHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     const { slug } = req.query;
     const streamRes = await redis.get(`${slug}`);
@@ -17,13 +33,22 @@ const getHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     return handleErrors(res, new Error('Stream not found'));
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//     if (req.method === 'GET') {
+//         return await getHandler(req, res);
+//     } else {
+//         return handleErrors(res, new Error('Method not allowed'));
+//     }
+// }
+
+export default allowCors(async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'GET') {
         return await getHandler(req, res);
     } else {
         return handleErrors(res, new Error('Method not allowed'));
     }
-}
+});
+
 
 process.on('SIGTERM', () => {
     redis.disconnect();
