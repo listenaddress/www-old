97692517d7154c54abe605cc19bf724602fd28b4
Thomas O'Brien <tbobrien612@gmail.com>
2023-08-01 13:50:22 -0500

    Add onboarding stepper

--- pages/onboarding.tsx
+++ pages/onboarding.tsx
@@ -6,11 +6,24 @@ import React, { useContext } from 'react';
 
 export default function Onboarding() {
     const [step, setStep] = useState(1);
+    const numOfSteps = 3;
     const [name, setName] = useState('');
     const { user, setUser, loadingUser } = useContext(GlobalContext);
     if (loadingUser) return <></>;
-    console.log('user', user)
+
     return (
-        <>Welcome to Steams!</>
+        <>
+            <div className='m-auto'>
+                <div className='flex justify-center mt-6'>
+                    {
+                        Array.from(Array(3).keys()).map((i) => {
+                            return (
+                                <div key={i} className={`inline-block w-[50px] h-[4px] bg-gray-300 ${i + 1 < step ? 'bg-gray-800' : ''} ${i + 1 === step ? 'bg-gray-900' : ''} ${i + 1 !== numOfSteps ? 'mr-2' : ''}`}></div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </>
     )
 }
