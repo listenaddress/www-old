be14e21444afee7354dd5630ca070ce9018aed22
Thomas O'Brien <tbobrien612@gmail.com>
2023-08-09 10:48:00 -0500

    Init hovering states and more options

--- pages/stream/[id]/index.tsx
+++ pages/stream/[id]/index.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { useState, useEffect, useContext } from 'react'
 import { parseContentForTable } from '@/lib/helpers';
-import { ArrowUpRightIcon, ChevronDownIcon, PencilSquareIcon, InformationCircleIcon } from '@heroicons/react/24/outline';
+import { ArrowUpRightIcon, ChevronDownIcon, PencilSquareIcon, InformationCircleIcon, HandThumbDownIcon, HandThumbUpIcon, LinkIcon } from '@heroicons/react/24/outline';
+import { HandThumbDownIcon as HandThumbDownIconSolid, HandThumbUpIcon as HandThumbUpIconSolid, LinkIcon as LinkIconSolid } from '@heroicons/react/24/solid';
 import { useRouter } from 'next/router'
 import { GlobalContext } from '@/context/store';
 import Skeleton from 'react-loading-skeleton';
@@ -13,6 +14,8 @@ import Link from 'next/link';
 export default function Stream() {
     const [stream, setStream] = useState({ name: '', description: '', created_by: '', created_at: '', updated_at: '' })
     const [content, setContent] = useState([])
+    const [hoveringIndex, setHoveringIndex] = useState(-1)
+    const [hoveringIndexMoreOptions, setHoveringIndexMoreOptions] = useState(-1)
     const [moreOptionsHover, setMoreOptionsHover] = useState(-1)
     const [contentFeedback, setContentFeedback] = useState({})
     const router = useRouter()
@@ -122,35 +125,68 @@ export default function Stream() {
                     ) : (
                         <div className='text-[13px]'>
                             {content.map((item: any, index: number) => (
-                                <Link href={item.url} key={index}>
-                                    <div key={index} className='flex items-start pb-5 pt-5 border-b-2'>
-                                        <div className='inline-block w-1/5 sm:w-[8%] lg:w-[22%] xl:w-[19%] pr-2'>
-                                            <div className='rounded-full w-7 h-7 absolute'>
-                                                {
-                                                    item.ico ? (
-                                                        <img className='w-7 h-7 rounded-md' src={item.ico} />
-                                                    ) : (
-                                                        <img className='w-7 h-7 rounded-md' src={item.platformImage || '/platform-images/paper.png'} />
-                                                    )
-                                                }
+                                <div className='relative hover:bg-gray-300 ml-[-1rem] pl-[1rem] mr-[-1rem] pr-[1rem] hover:mt-[-1px] hover:pt-[1px]' key={index} onMouseEnter={() => setHoveringIndex(index)} onMouseLeave={() => setHoveringIndex(-1)}>
+                                    <Link href={item.url}>
+                                        <div className=''>
+                                            <div key={index} className='flex items-start pb-5 pt-5 border-b-2 hover:border-transparent min-h-[70px]'>
+                                                <div className='inline-block w-1/5 sm:w-[8%] lg:w-[22%] xl:w-[19%] pr-2'>
+                                                    <div className='rounded-full w-7 h-7 absolute'>
+                                                        {
+                                                            item.ico ? (
+                                                                <img className='w-7 h-7 rounded-md' src={item.ico || '/platform-images/paper.png'} alt="/platform-images/paper.png" />
+                                                            ) : (
+                                                                <img className='w-7 h-7 rounded-md' src={item.platformImage || '/platform-images/paper.png'} alt="/platform-images/paper.png" />
+                                                            )
+                                                        }
+                                                    </div>
+                                                    <span className='hidden lg:inline-block bottom-[-4px] left-[40px] pr-[50px] relative'>
+                                                        {item.venue}
+                                                    </span>
+                                                </div>
+                                                <div className='inline-block w-5/6 sm:w-[42%] lg:w-[40%] xl:w-[42%] mt-[4px] pr-3'>
+                                                    <div className=''>
+                                                        {item.title}
+                                                    </div>
+                                                </div>
+                                                <div className='hidden sm:inline-block sm:w-[35%] lg:w-[28%] xl:w-[32%] mt-[4px] pr-2'>
+                                                    {formatAuthors(item.authors)}
+                                                </div>
+                                                <div className='hidden sm:inline-block sm:w-[15%] lg:w-[15%] xl:w-[12%] text-right'>
+                                                    {item.time}
+                                                </div>
                                             </div>
-                                            <span className='hidden lg:inline-block bottom-[-4px] left-[40px] pr-[50px] relative'>
-                                                {item.venue}
-                                            </span>
                                         </div>
-                                        <div className='inline-block w-5/6 sm:w-[42%] lg:w-[40%] xl:w-[42%] mt-[4px] pr-3'>
-                                            <div className=''>
-                                                {item.title}
+                                    </Link>
+                                    {
+                                        hoveringIndex === index && (
+                                            <div className='absolute right-0 top-[14px] bg-gray-300 rounded-lg'>
+                                                <div className='flex items-center h-full'>
+                                                    <div onMouseEnter={() => setHoveringIndexMoreOptions(0)} onMouseLeave={() => setHoveringIndexMoreOptions(-1)} className='cursor-pointer'>
+                                                        <LinkIcon className={`w-10 h-10 pl-3 pr-2 pt-1 pb-1 ${hoveringIndexMoreOptions === 0 ? 'stroke-[2.3px]' : ''}`} />
+                                                    </div>
+                                                    <div onMouseEnter={() => setHoveringIndexMoreOptions(1)} onMouseLeave={() => setHoveringIndexMoreOptions(-1)} className='cursor-pointer'>
+                                                        {
+                                                            hoveringIndexMoreOptions !== 1 ? (
+                                                                <HandThumbUpIcon className='w-10 h-10 pl-2 pr-2 pt-1 pb-1' />
+                                                            ) : (
+                                                                <HandThumbUpIconSolid className='w-10 h-10 pl-2 pr-2 pt-1 pb-1' />
+                                                            )
+                                                        }
+                                                    </div>
+                                                    <div onMouseEnter={() => setHoveringIndexMoreOptions(2)} onMouseLeave={() => setHoveringIndexMoreOptions(-1)} className='cursor-pointer'>
+                                                        {
+                                                            hoveringIndexMoreOptions !== 2 ? (
+                                                                <HandThumbDownIcon className='w-10 h-10 pl-2 pr-2 pt-1 pb-1' />
+                                                            ) : (
+                                                                <HandThumbDownIconSolid className='w-10 h-10 pl-2 pr-2 pt-1 pb-1' />
+                                                            )
+                                                        }
+                                                    </div>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className='hidden sm:inline-block sm:w-[35%] lg:w-[28%] xl:w-[32%] mt-[4px] pr-2'>
-                                            {formatAuthors(item.authors)}
-                                        </div>
-                                        <div className='hidden sm:inline-block sm:w-[15%] lg:w-[15%] xl:w-[12%] text-right'>
-                                            {item.time}
-                                        </div>
-                                    </div>
-                                </Link>
+                                        )
+                                    }
+                                </div>
                             ))}
                         </div>
                     )
