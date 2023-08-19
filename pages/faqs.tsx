e1ca41369cdc03a9397a361ba0a249a1d882bff6
Thomas O'Brien <tbobrien612@gmail.com>
2023-08-19 09:31:49 -0500

    Clean up faqs

--- pages/faqs.tsx
+++ pages/faqs.tsx
@@ -11,15 +11,15 @@ export default function FAQs() {
             <div className='m-4 mb-8 sm:ml-20 mt-[19px]'>
                 <div className={`text-lg font-medium`}>FAQs</div>
                 {/* Center the div, left align the text */}
-                <div className={`items-center text-left max-w-[500px] mx-auto mt-6`}>
-                    <span className='text-sm font-medium'>What is Streams?</span>
-                    <div className='text-sm mb-4'>Streams is an app of content from a website or other source that can be read in an RSS reader.</div>
-                    <span className='text-sm font-medium'>What is a stream?</span>
-                    <div className='text-sm mb-4'>Streams is an app of content from a website or other source that can be read in an RSS reader.</div>
-                    <span className='text-sm font-medium'>How much will streams cost?</span>
-                    <div className='text-sm mb-4'>We’re not sure. We’re testing out somethings, will let you know!</div>
-                    <span className='text-sm font-medium'>Who is making this?</span>
-                    <div className='text-sm mb-4'>Streams is an app of content from a website or other source that can be read in an RSS reader.</div>
+                <div className={`items-center text-left max-w-[500px] mx-auto mt-10`}>
+                    <span className='font-bold'>What is Streams?</span>
+                    <div className='mb-6'>Streams is an app of content from a website or other source that can be read in an RSS reader.</div>
+                    <span className='font-bold'>What is a stream?</span>
+                    <div className='mb-6'>Streams is an app of content from a website or other source that can be read in an RSS reader.</div>
+                    <span className='font-bold'>How much will streams cost?</span>
+                    <div className='mb-6'>We’re not sure. We’re testing out somethings, will let you know!</div>
+                    <span className='font-bold'>Who is making this?</span>
+                    <div className='mb-6'>Streams is an app of content from a website or other source that can be read in an RSS reader.</div>
                 </div>
             </div>
         </>
