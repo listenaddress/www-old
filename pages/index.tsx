155a69eef0ae6692809bdd64be919f5e94704ea0
Thomas O'Brien <tbobrien612@gmail.com>
2023-10-04 18:40:03 -0500

    Get rid of links

--- pages/index.tsx
+++ pages/index.tsx
@@ -266,7 +266,33 @@ export default function Home() {
                   )
                 })
               }
-
+              <div className='mt-8'>
+                <Button
+                  onClick={signIn}
+                >
+                  + Make your own stream
+                </Button>
+              </div>
+              <div>
+                <div className='font-medium mb-4 text-gray-500 mt-44'>
+                  Look for clues
+                </div>
+                <div className='font-medium mb-4'>
+                  <p className="mb-3">Everyday, we scan the web for you, collecting things you may need.</p>
+                  <p className="mb-3">Anytime you want, you can pop in to get something new to read, listen to, or watch.</p>
+                  <p className="mb-3">Sometimes you find a direct answer to a question. Sometimes you find new questions. And other times you find material you're able to build on in your current work.</p>
+                  <p className="mb-3">This website is made to provide you with the clues you need—whether you're exploring new areas of science, solving a health issue, or writing your next paper.</p>
+                  <p className="">We're just starting to test out the waters. <Link href="/sign-in" className='underline'>Join us.</Link></p>
+                  {/* <Image
+                    src='/stream-geese.jpeg'
+                    className='my-7'
+                    alt='testing-stream'
+                    width={320}
+                    height={320}
+                  /> */}
+                </div>
+                <div className='font-medium mb-4 text-gray-500 mt-40'>Links</div>
+              </div>
             </main>
           </>
         )
