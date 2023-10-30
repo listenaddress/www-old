8b496fd7b4b86f2b212d1579f869bbb2dbb3e4c4
Thomas O'Brien <tbobrien612@gmail.com>
2023-10-30 12:19:27 -0500

    Rm mobilemenu for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,15 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@/context/theme'
 import { GlobalContextProvider } from '@/context/store'
-import SideBar from '@/components/sideBar'
-import MobileMenu from '@/components/mobileMenu'
+import TopBar from '@/components/topBar'
+// import MobileMenu from '@/components/mobileMenu'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <GlobalContextProvider>
       <ThemeProvider>
-        <MobileMenu />
-        <SideBar />
+        {/* <MobileMenu /> */}
+        <TopBar />
         <Component {...pageProps} />
       </ThemeProvider >
     </GlobalContextProvider >
